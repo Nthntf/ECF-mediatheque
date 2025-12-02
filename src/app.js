85f9e80b2c7e74370dc63035732b2d97b900b475
theo feuerstose <theo.feu54@gmail.com>
2025-12-02 11:29:32 +0100

    added light/dark theme controls w/ jQuery

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import $ from "jquery";
+import $, { param } from "jquery";
 
 let films = [
     {
@@ -23,3 +23,31 @@ let films = [
     },
 ];
 // TODO
+
+// Montre ou cache la section pour ajouter un film en fonction de sa classe déjà présente
+$("#add-table-btn").on("click", () => {
+    const tableSection = $("#add-table-section");
+    if (tableSection.hasClass("hidden")) {
+        tableSection.removeClass("hidden").addClass("flex");
+    } else {
+        tableSection.removeClass("flex").addClass("hidden");
+    }
+});
+
+// Gère le thème clair/sombre
+$("#light-dark-btn").on("click", () => {
+    const sunSVG = $("#THESUN");
+    const moonSVG = $("#THEMOON");
+    const html = $("html");
+    if (sunSVG.hasClass("hidden")) {
+        // Remplace la lune par le soleil et ajoute le thème clair sur <html>
+        sunSVG.removeClass("hidden").addClass("flex");
+        moonSVG.removeClass("flex").addClass("hidden");
+        html.removeClass("dark");
+    } else {
+        // Remplace le soleil par la lune et ajoute le thème sombre sur <html>
+        moonSVG.removeClass("hidden").addClass("flex");
+        sunSVG.removeClass("flex").addClass("hidden");
+        html.addClass("dark");
+    }
+});
