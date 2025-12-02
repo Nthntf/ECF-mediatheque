f36f5956683b4df6c7a83a29c523c9884b2d16b2
theo feuerstose <theo.feu54@gmail.com>
2025-12-02 15:37:13 +0100

    added filter

--- src/app.js
+++ src/app.js
@@ -22,20 +22,37 @@ let films = [
         authors: "Andy Muschietti",
     },
 ];
-// TODO
 
-const select = $("#filter-select");
-select.on("change", (e) => {
-    console.log(select.val());
-});
+// boucle sur tout le tableau filtré (pas filtré au lancement du site)
+function renderTable(data) {
+    $("#movie-table").html("");
+    data.forEach((movie) => {
+        $("#movie-table").append(`
+            <tr class="h-16">
+                <td class="table-cell">${movie.title}</td>
+                <td class="table-cell">${movie.years}</td>
+                <td class="table-cell">${movie.authors}</td>
+            </tr>
+        `);
+    });
+}
 
-// Ajoute chaque element de 'films' dans #movie-table
-films.forEach((movie) => {
-    $("#movie-table").append(`<tr class="h-16">
-                        <td class="table-cell">${movie.title}</td>
-                        <td class="table-cell">${movie.years}</td>
-                        <td class="table-cell">${movie.authors}</td>
-                    </tr>`);
+renderTable(films);
+
+// créer un nouveau tableau avec conditions de filtre en fonction de <select>.value
+$("#filter-select").on("change", () => {
+    const filterValue = $("#filter-select").val();
+    let sortedMovies = [...films];
+
+    if (filterValue === "movie-name") {
+        sortedMovies.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (filterValue === "year") {
+        sortedMovies.sort((a, b) => a.years - b.years);
+    } else if (filterValue === "author-name") {
+        sortedMovies.sort((a, b) => a.authors.localeCompare(b.authors));
+    }
+
+    renderTable(sortedMovies);
 });
 
 // Montre/cache la section pour ajouter un film en fonction de sa classe présente
